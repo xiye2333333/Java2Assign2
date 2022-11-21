60533e42029979431873e8ea5b55ab94a0be14d3
xiye2333333 <1072689964@qq.com>
2022-11-21 19:00:09 +0800

    Update Main.java

--- Tic-tac-toe/src/application/Main.java
+++ Tic-tac-toe/src/application/Main.java
@@ -1,70 +1,65 @@
 package application;
 
 import application.controller.Controller;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.Socket;
-
 public class Main extends Application {
-    String serverName = "127.0.0.1";
-    int serverPort = 8080;
-    Socket client = null;
-    //client
-    @Override
-    public void start(Stage primaryStage) {
+  String serverName = "127.0.0.1";
+  int serverPort = 8080;
+  Socket client = null;
 
-        try {
-            client = new Socket(serverName, serverPort);
-            InputStream inFromServer = client.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
+  //client
+  @Override
+  public void start(Stage primaryStage) {
 
-            System.out.println(in.readUTF());
+    try {
+      client = new Socket(serverName, serverPort);
+      InputStream inFromServer = client.getInputStream();
+      DataInputStream in = new DataInputStream(inFromServer);
+
+      System.out.println(in.readUTF());
 //            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-
-            fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
-            Pane root = fxmlLoader.load();
-            primaryStage.setTitle("Tic Tac Toe");
-            primaryStage.setScene(new Scene(root));
-            primaryStage.setResizable(false);
-
-            primaryStage.show();
-
-            Controller controller = fxmlLoader.getController();
-            controller.setSocket(client);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public void stop() throws Exception {
-        super.stop();
-        System.exit(0);
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+
+      fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
+      Pane root = fxmlLoader.load();
+      primaryStage.setTitle("Tic Tac Toe");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.setResizable(false);
+
+      primaryStage.show();
+
+      Controller controller = fxmlLoader.getController();
+      controller.setSocket(client);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
+  @Override
+  public void stop() throws Exception {
+    super.stop();
+    System.exit(0);
+  }
 
-//        for (int i = 0; i < 11; i++) {
-//            Thread thread = new Thread(new client());
-//            thread.start();
-//        }
-        launch(args);
+  public static void main(String[] args) {
 
-    }
+    launch(args);
+
+  }
 }
